cc970fe15a86ebac0e800c14a92c6fedb1725277
arsyadn <arsyadnurzaky@gmail.com>
2025-11-10 16:15:28 +0700

    feat(home): add several codes

--- src/component/AuthForm.tsx
+++ src/component/AuthForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Button, Input, Form, Typography, Spin, message, Alert } from "antd";
+import { Button, Input, Form, Typography, Spin, Alert } from "antd";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
